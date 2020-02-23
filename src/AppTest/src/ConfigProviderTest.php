3f9725af34833e0995ad693918513ed4a70f7049
Alexander S <alexander@sharapov.biz>
2020-02-23 23:48:42 +0600

    Adding ConfigProviderTests

---
+++ src/AppTest/src/ConfigProviderTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @copyright Sharapov A. <alexander@sharapov.biz>
+ * @link      http://www.sharapov.biz/
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
+ * Date: 23.02.2020
+ * Time: 23:35
+ */
+
+declare(strict_types=1);
+
+namespace AppTest;
+
+use App\ConfigProvider;
+use Mezzio\Hal\Metadata\MetadataMap;
+use PHPUnit\Framework\TestCase;
+
+class ConfigProviderTest extends TestCase
+{
+    /**
+     * @var ConfigProvider
+     */
+    private $provider;
+
+    protected function setUp(): void
+    {
+        $this->provider = new ConfigProvider();
+    }
+
+    public function testInvocationReturnsArray(): array
+    {
+        $config = ($this->provider)();
+        self::assertIsArray($config);
+
+        return $config;
+    }
+
+    /**
+     * @depends testInvocationReturnsArray
+     */
+    public function testReturnedArrayContainsDependencies(array $config): void
+    {
+        self::assertArrayHasKey('dependencies', $config);
+        self::assertArrayHasKey('doctrine', $config);
+        self::assertArrayHasKey(MetadataMap::class, $config);
+        self::assertArrayHasKey('hydrators', $config);
+        self::assertIsArray($config['dependencies']);
+        self::assertIsArray($config['doctrine']);
+        self::assertIsArray($config[MetadataMap::class]);
+        self::assertIsArray($config['hydrators']);
+    }
+}
